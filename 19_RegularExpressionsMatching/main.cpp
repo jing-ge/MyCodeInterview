a757e38a23f312331bbf27bbf6ce04d4d089653d
jing-ge <675324370@qq.com>
2020-03-26 17:46:41 +0800

    finish problem 19

---
+++ 19_RegularExpressionsMatching/main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+
+using namespace std;
+
+#include <cstdio>
+
+bool matchCore(const char* str, const char* pattern);
+
+bool match(const char* str, const char* pattern)
+{
+    if(str == nullptr || pattern == nullptr)
+        return false;
+
+    return matchCore(str, pattern);
+}
+
+bool matchCore(const char* str, const char* pattern)
+{
+    if(*str == '\0' && *pattern == '\0')
+        return true;
+
+    if(*str != '\0' && *pattern == '\0')
+        return false;
+
+    if(*(pattern + 1) == '*')
+    {
+        if(*pattern == *str || (*pattern == '.' && *str != '\0'))
+            // 进入有限状态机的下一个状态
+            return matchCore(str + 1, pattern + 2)
+            // 继续留在有限状态机的当前状态
+            || matchCore(str + 1, pattern)
+            // 略过一个'*'
+            || matchCore(str, pattern + 2);
+        else
+            // 略过一个'*'
+            return matchCore(str, pattern + 2);
+    }
+
+    if(*str == *pattern || (*pattern == '.' && *str != '\0'))
+        return matchCore(str + 1, pattern + 1);
+
+    return false;
+}
+bool mymatchCore(const char* str,const char* pattern)
+{
+    if((*str)=='\0' && (*pattern=='\0'))return true;
+    if((*str)!='\0' && (*pattern=='\0'))return false;
+
+    if(*(pattern+1)=='*')
+    {
+        if(*pattern == *str || (*pattern == '.' && *str != '\0'))
+            return mymatchCore(str+1,pattern+2)|| mymatchCore(str+1,pattern)||mymatchCore(str,pattern+2);
+        else
+            return mymatchCore(str,pattern+2);
+    }
+    if(*str==*pattern||(*pattern=='.'&&*str!='\0'))
+        return mymatchCore(str+1,pattern+1);
+    return false;
+}
+bool mymatch(const char* str,const char* pattern)
+{
+    if(str==nullptr||pattern==nullptr)return false;
+    return mymatchCore(str,pattern);
+}
+
+
+// ====================测试代码====================
+void Test(const char* testName, const char* string, const char* pattern, bool expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    if(mymatch(string, pattern) == expected)
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+int main(int argc, char* argv[])
+{
+    Test("Test01", "", "", true);
+    Test("Test02", "", ".*", true);
+    Test("Test03", "", ".", false);
+    Test("Test04", "", "c*", true);
+    Test("Test05", "a", ".*", true);
+    Test("Test06", "a", "a.", false);
+    Test("Test07", "a", "", false);
+    Test("Test08", "a", ".", true);
+    Test("Test09", "a", "ab*", true);
+    Test("Test10", "a", "ab*a", false);
+    Test("Test11", "aa", "aa", true);
+    Test("Test12", "aa", "a*", true);
+    Test("Test13", "aa", ".*", true);
+    Test("Test14", "aa", ".", false);
+    Test("Test15", "ab", ".*", true);
+    Test("Test16", "ab", ".*", true);
+    Test("Test17", "aaa", "aa*", true);
+    Test("Test18", "aaa", "aa.a", false);
+    Test("Test19", "aaa", "a.a", true);
+    Test("Test20", "aaa", ".a", false);
+    Test("Test21", "aaa", "a*a", true);
+    Test("Test22", "aaa", "ab*a", false);
+    Test("Test23", "aaa", "ab*ac*a", true);
+    Test("Test24", "aaa", "ab*a*c*a", true);
+    Test("Test25", "aaa", ".*", true);
+    Test("Test26", "aab", "c*a*b", true);
+    Test("Test27", "aaca", "ab*a*c*a", true);
+    Test("Test28", "aaba", "ab*a*c*a", false);
+    Test("Test29", "bbbba", ".*a*a", true);
+    Test("Test30", "bcbbabab", ".*a*a", false);
+
+    return 0;
+}
